9c81856d1c4260997643a53e60bd96648cb65e18
Pranskus <160307694+Pranskus@users.noreply.github.com>
2025-02-25 11:35:20 +0200

    .

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-scroll";
-import { UilSearch, UilShoppingCart, UilUser } from "./IconWrappers";
+import { UilShoppingCart, UilUser } from "./IconWrappers";
 import styled from "styled-components";
 
 const AnimatedCartButton = styled.button`
@@ -169,38 +169,14 @@ const Header: React.FC<HeaderProps> = ({
   currentPage,
   onGoBack,
 }) => {
-  const [isSearchActive, setIsSearchActive] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isMobileCategoriesOpen, setIsMobileCategoriesOpen] = useState(false);
   const [showMenCategories, setShowMenCategories] = useState(false);
   const [showWomenCategories, setShowWomenCategories] = useState(false);
-  const searchRef = useRef<HTMLDivElement>(null);
   const dropdownRef = useRef<HTMLDivElement>(null);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const [mobileSearchActive, setMobileSearchActive] = useState(false);
   const [showFloatingCart, setShowFloatingCart] = useState(false);
   const [isCartAnimating, setIsCartAnimating] = useState(false);
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        searchRef.current &&
-        !searchRef.current.contains(event.target as Node) &&
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
-      ) {
-        setIsSearchActive(false);
-        setIsDropdownOpen(false);
-        setShowMenCategories(false);
-        setShowWomenCategories(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -261,7 +237,6 @@ const Header: React.FC<HeaderProps> = ({
             padding: "0 20px",
           }}
         >
-          {}
           <div className="flex-1 flex items-center justify-start">
             <HamburgerIcon
               onClick={toggleMobileMenu}
@@ -350,7 +325,6 @@ const Header: React.FC<HeaderProps> = ({
             </nav>
           </div>
 
-          {}
           <div
             onClick={onLogoClick}
             className="flex-shrink-0 flex justify-center items-center"
@@ -366,38 +340,7 @@ const Header: React.FC<HeaderProps> = ({
             />
           </div>
 
-          {}
           <div className="flex-1 flex items-center justify-end space-x-4">
-            <div
-              className="relative hidden lg:flex items-center"
-              ref={searchRef}
-            >
-              <button
-                onClick={() => setIsSearchActive(!isSearchActive)}
-                className="rounded-full focus:outline-none"
-                style={{ padding: "8px" }}
-              >
-                <UilSearch
-                  style={{
-                    width: "24px",
-                    height: "24px",
-                  }}
-                />
-              </button>
-              {isSearchActive && (
-                <input
-                  type="text"
-                  className="border rounded-full absolute right-0 top-full mt-2"
-                  style={{
-                    padding: "8px 16px 8px 40px",
-                    width: "200px",
-                    fontSize: "14px",
-                  }}
-                  placeholder="Search..."
-                  onBlur={() => setIsSearchActive(false)}
-                />
-              )}
-            </div>
             <MobileCartButton onClick={handleCartClick} className="lg:p-2">
               <UilShoppingCart className="w-8 h-8 lg:w-6 lg:h-6 cart-icon" />
               <span
@@ -413,14 +356,9 @@ const Header: React.FC<HeaderProps> = ({
                 {cartCount}
               </span>
             </MobileCartButton>
-            <button
-              className="hidden lg:flex items-center rounded-full focus:outline-none"
-              style={{ padding: "8px" }}
-            ></button>
           </div>
         </header>
 
-        {}
         <MobileMenu open={isMobileMenuOpen}>
           <CloseButton onClick={toggleMobileMenu}>&times;</CloseButton>
           <div className="p-4 w-full">
@@ -435,8 +373,6 @@ const Header: React.FC<HeaderProps> = ({
               New Drops <AnimatedFire>🔥</AnimatedFire>
             </Link>
             <div className="py-6">
-              {" "}
-              {}
               <button
                 className="text-xl font-bold"
                 onClick={() => {
@@ -447,8 +383,6 @@ const Header: React.FC<HeaderProps> = ({
               </button>
               {isMobileCategoriesOpen && (
                 <div className="mt-4">
-                  {" "}
-                  {}
                   <button
                     className="block py-3 w-full text-lg font-bold"
                     onClick={() => {
@@ -474,31 +408,10 @@ const Header: React.FC<HeaderProps> = ({
                 </div>
               )}
             </div>
-            <div className="py-6">
-              {" "}
-              {}
-              <button
-                className="text-xl font-bold flex items-center justify-center w-full"
-                onClick={() => setMobileSearchActive(!mobileSearchActive)}
-              >
-                <UilSearch
-                  style={{ width: "24px", height: "24px", marginRight: "8px" }}
-                />
-                Search
-              </button>
-              {mobileSearchActive && (
-                <input
-                  type="text"
-                  className="mt-4 w-full border rounded-full px-4 py-3 text-center"
-                  placeholder="Search..."
-                />
-              )}
-            </div>
           </div>
         </MobileMenu>
       </div>
 
-      {/* Floating Cart */}
       <div
         className={`fixed bottom-8 right-8 transition-all duration-300 transform z-[9999] ${
           showFloatingCart && currentPage !== "cart"
